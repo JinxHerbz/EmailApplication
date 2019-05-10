2e9e3992cdc2790cdf44cefcd60768e5fd6a0770
chris <chris@LAPTOP-A4PH2AQS.connect>
2019-05-10 23:35:51 +0100

    First Commit

---
+++ EmailApplication/src/emailapp/EmailApp.java
@@ -0,0 +1,12 @@
+package emailapp;
+import java.util.Random;
+
+public class EmailApp {
+	
+	public static void main(String[] args) {
+		
+		Email email1 = new Email("joe","herbert");
+		System.out.println(email1.showInfo());
+	
+	}
+}
